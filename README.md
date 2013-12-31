c07288c837e0881eaf19371efad8125cf4d42e5d
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-31 16:31:07 +0100

    Updating the storm project

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 storm-hellocounter
 ==================
 
-Storm topology for counting words
+Storm topology for counting words.
+
+
+
+## Running
+
+* Execute the command: mvn -f pom.xml compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass="TopologyMain" -Dexec.args="src/main/resources/words.txt"
--- src/main/java/TopologyMain.java
+++ src/main/java/TopologyMain.java
@@ -10,28 +10,27 @@ import spouts.WordReader;
  * User: domenicosolazzo
  */
 public class TopologyMain {
-    public static void main(String[] args) throws InterruptedException{
-        // Topology definition
-        TopologyBuilder topology =new TopologyBuilder();
-        topology.setSpout("word-reader", new WordReader());
-        topology.setBolt("word-normalizer", new WordNormalizer()).shuffleGrouping("word-reader");
-        topology.setBolt("word-counter", new WordCounter(), 2).fieldsGrouping("word-normalizer", new Fields("word"));
+    public static void main(String[] args) throws InterruptedException {
 
+        //Topology definition
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("word-reader",new WordReader());
 
-        // Configuration
+        builder.setBolt("word-normalizer", new WordNormalizer())
+                .shuffleGrouping("word-reader");
+        builder.setBolt("word-counter", new WordCounter(),2)
+                .fieldsGrouping("word-normalizer", new Fields("word"));
+
+        //Configuration
         Config conf = new Config();
         conf.put("wordsFile", args[0]);
-        conf.setDebug(true);
-
-        // Topology run
+        conf.setDebug(false);
+        //Topology run
         conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
         LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("Submit Word count topology", conf, topology.createTopology());
-
-        // Wait 2 seconds
-        Thread.sleep(2000);
-        // Shutdown
+        cluster.submitTopology("Getting-Started-Toplogie", conf,
+                builder.createTopology());
+        Thread.sleep(1000);
         cluster.shutdown();
     }
-
 }
